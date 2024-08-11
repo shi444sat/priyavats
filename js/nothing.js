c1c0fa95c0f857b8f4927f9f7cfa730e51a8bb8d
shi444sat <89733538+shi444sat@users.noreply.github.com>
2024-08-11 21:56:44 +0530

    nothing.js

--- js/nothing.js
+++ js/nothing.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   // Check if the current URL contains the query parameter "text"
   const urlParams = new URLSearchParams(window.location.search);
   if (urlParams.has("love")) {
-    window.location.href = 'https://priyavats.onrender.com/?room=love';
+    window.location.href = 'https://priyavats.onrender.com/?room=1205';
   }
 });
