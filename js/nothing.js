56effc57e55cc409b143f5c31fce0a13f909db69
shi444sat <89733538+shi444sat@users.noreply.github.com>
2024-08-31 21:23:53 +0530

    nothing.js

--- js/nothing.js
+++ js/nothing.js
@@ -2,6 +2,15 @@ document.addEventListener("DOMContentLoaded", () => {
   // Check if the current URL contains the query parameter "text"
   const urlParams = new URLSearchParams(window.location.search);
   if (urlParams.has("shyari")) {
-    window.location.href = 'https://priyavats.onrender.com/?room=1205'
+    window.location.href = 'https://priyavats.onrender.com/?room=1205';
+  }
+});
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Check if the current URL contains the query parameter "text"
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has("shyarii")) {
+    window.location.href = 'https://priyavats.onrender.com/?room=12055';
   }
 });
