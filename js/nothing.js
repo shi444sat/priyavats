1e6be510f63fc4d65516e838e469c54959e3d66c
shi444sat <89733538+shi444sat@users.noreply.github.com>
2024-08-31 21:28:28 +0530

    nothing.js

--- js/nothing.js
+++ js/nothing.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Check if the current URL contains the query parameter "text"
+  // Get the current URL's query parameters
   const urlParams = new URLSearchParams(window.location.search);
-  if (urlParams.has("shyari")) {
-    window.location.href = 'https://priyavats.onrender.com/?room=1205';
-  }
-});
+  
+  // Check if there is at least one query parameter
+  if (urlParams.toString()) {
+    // Get the first query parameter name
+    const firstParam = urlParams.keys().next().value;
 
-
-document.addEventListener("DOMContentLoaded", () => {
-  // Check if the current URL contains the query parameter "text"
-  const urlParams = new URLSearchParams(window.location.search);
-  if (urlParams.has("shyarii")) {
-    window.location.href = 'https://priyavats.onrender.com/?room=12055';
+    // If a parameter exists, redirect to the new URL with that parameter value + '01'
+    if (firstParam) {
+      const newUrl = `https://priyavats.onrender.com/?room=${firstParam}01`;
+      window.location.href = newUrl;
+    }
   }
 });
