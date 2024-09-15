5e6ead40700145fb9e5c3523cbfca00286c31da7
shi444sat <89733538+shi444sat@users.noreply.github.com>
2024-09-15 22:42:18 +0530

    bkl.js

--- js/bkl.js
+++ js/bkl.js
@@ -1,13 +1,11 @@
-      // Your Firebase configuration
-      const firebaseConfig = {
-        apiKey: "AIzaSyDfIqRZcSTqT7s1w3NNq1yZoQyrcdKOjF8",
-        authDomain: "suhani-96c78.firebaseapp.com",
-        projectId: "suhani-96c78",
-        storageBucket: "suhani-96c78.appspot.com",
-        messagingSenderId: "275036106574",
-        appId: "1:275036106574:web:c6f769e038aa89795f781f"
-      };
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
 
-      // Initialize Firebase
-      const app = firebase.initializeApp(firebaseConfig);
-      const db = firebase.firestore(app);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
