97ed36c708d6bf98687d2dc3257a4929a7550d46
shi444sat <89733538+shi444sat@users.noreply.github.com>
2024-10-01 22:26:03 +0530

    Add files via upload

---
+++ js/nothing.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Get the current URL's query parameters
+  const urlParams = new URLSearchParams(window.location.search);
+  
+  // Check if there is at least one query parameter
+  if (urlParams.toString()) {
+    // Get the first query parameter name
+    const firstParam = urlParams.keys().next().value;
+
+    // If a parameter exists, redirect to the new URL with that parameter value + '01'
+    if (firstParam) {
+      const newUrl = `https://priyavats.onrender.com/?room=${firstParam}`;
+      window.location.href = newUrl;
+    }
+  }
+});
